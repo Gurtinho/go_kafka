cb3df3f9455917a221dff0907231db2c621a9ccc
gustavo litter <gustavolitter@gmail.com>
2024-09-29 20:07:03 -0300

    feat(main): Criado o consumidor para receber as mensagens do kafka

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -1,2 +1,34 @@
-package consumer
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+	configMap := &kafka.ConfigMap{
+		"bootstrap.servers": "go_kafka-kafka-1:9092",
+		"client.id": "kafka_consumer",
+		"group.id": "kafka_group",
+		"auto.offset.reset": "earliest",
+	}
+
+	c, err := kafka.NewConsumer(configMap)
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	topics := []string{"teste"}
+	c.SubscribeTopics(topics, nil)
+
+	for {
+		msg, err := c.ReadMessage(-1)
+		if err == nil {
+			fmt.Println(string(msg.Value), msg.TopicPartition)
+		}
+	}
+}
+
 
